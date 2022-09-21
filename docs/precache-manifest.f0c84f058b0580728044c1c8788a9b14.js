db096b18adb95b71f3ac9525a4bf29603688e7fd
princerohan225 <princerohan225@gmail.com>
2022-09-22 03:57:15 +0530

    initial commit

---
+++ docs/precache-manifest.f0c84f058b0580728044c1c8788a9b14.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "d430086b53af105aef8b",
+    "url": "/princerohan225/Gym.gits/static/css/main.b2268efb.chunk.css"
+  },
+  {
+    "revision": "d430086b53af105aef8b",
+    "url": "/princerohan225/Gym.gits/static/js/main.d430086b.chunk.js"
+  },
+  {
+    "revision": "e7e0e04a5ea3842eb42e",
+    "url": "/princerohan225/Gym.gits/static/js/1.e7e0e04a.chunk.js"
+  },
+  {
+    "revision": "4aa755ebaa12f3b44de9",
+    "url": "/princerohan225/Gym.gits/static/js/2.4aa755eb.chunk.js"
+  },
+  {
+    "revision": "6fb3a9b8e4a7e3b4142e",
+    "url": "/princerohan225/Gym.gits/static/js/runtime~main.6fb3a9b8.js"
+  },
+  {
+    "revision": "265d7407a36a0628a0aac01352ced1b0",
+    "url": "/princerohan225/Gym.gits/static/media/gym.265d7407.png"
+  },
+  {
+    "revision": "8ceb0edea0b0ab052a53cb7decc6e41e",
+    "url": "/princerohan225/Gym.gits/static/media/8.8ceb0ede.jpg"
+  },
+  {
+    "revision": "fe03ddc13dfc30c09f158195aae779d0",
+    "url": "/princerohan225/Gym.gits/static/media/amrit.fe03ddc1.jpg"
+  },
+  {
+    "revision": "1b8f1d38576a91f8e049ec9fdb51da88",
+    "url": "/princerohan225/Gym.gits/static/media/sahil.1b8f1d38.jpg"
+  },
+  {
+    "revision": "7db722165430f2bef1f29754672fe2df",
+    "url": "/princerohan225/Gym.gits/static/media/cardio.7db72216.png"
+  },
+  {
+    "revision": "8c33534eb3c136a8d1e6564c02c2df90",
+    "url": "/princerohan225/Gym.gits/static/media/strength.8c33534e.png"
+  },
+  {
+    "revision": "e485728055a64311843a855ddf8885d9",
+    "url": "/princerohan225/Gym.gits/static/media/aerobics.e4857280.png"
+  },
+  {
+    "revision": "8a1f137284140c24514f72790b7e669c",
+    "url": "/princerohan225/Gym.gits/index.html"
+  }
+];
--- src/App.js
+++ src/App.js
@@ -1,47 +1,48 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import About from './component/about';
 import Navbar from './component/navbar';
 import Text from './component/text'
 import Contact from './component/contact';
 import Packages from './component/packages';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, HashRouter } from "react-router-dom";
 import Facilities from './component/facilities';
 
 
 
 function App() {
-  const [ModeText, setModeText]=useState('Enable Dark Mode');
-  const [Mode, setMode]= useState('light')
+  const [ModeText, setModeText] = useState('Enable Dark Mode');
+  const [Mode, setMode] = useState('light')
 
-  const togglemode=()=>{
-    if (Mode!=='dark'){
+  const togglemode = () => {
+    if (Mode !== 'dark') {
       setMode('dark');
       setModeText('Enable Light Mode');
-      document.body.style.backgroundColor='black';
-      document.body.style.color='white'
+      document.body.style.backgroundColor = 'black';
+      document.body.style.color = 'white'
     }
-    else{
+    else {
       setMode('light');
       setModeText('Enable Dark Mode');
-      document.body.style.backgroundColor='white';
-      document.body.style.color='black'
+      document.body.style.backgroundColor = 'white';
+      document.body.style.color = 'black'
     }
   }
   return (
     <div className="App">
 
       <Router>
-        <Navbar title={'The gym'} HomeText={'Home'} ContactText={'Contact'} PackagesText={'Packages'} FacilitiesText={'Facilities'} AboutText={'About'} modetext={ModeText} mode={Mode} togglemode={togglemode}/>
+        <HashRouter>
+          <Navbar title={'The gym'} HomeText={'Home'} ContactText={'Contact'} PackagesText={'Packages'} FacilitiesText={'Facilities'} AboutText={'About'} modetext={ModeText} mode={Mode} togglemode={togglemode} />
 
-        <Routes>
-          <Route exact path='/' element={<Text />} />
+          <Routes>
+            <Route exact path='/' element={<Text />} />
 
-          <Route exact path='/about' element={<About />} />
-          <Route exact path='/contact' element={<Contact/>} />
-          <Route exact path='/packages' element={<Packages/>} />
-          <Route exact path='/facilities' element={<Facilities/>} />
-        </Routes>
-
+            <Route exact path='/about' element={<About />} />
+            <Route exact path='/contact' element={<Contact />} />
+            <Route exact path='/packages' element={<Packages />} />
+            <Route exact path='/facilities' element={<Facilities />} />
+          </Routes>
+        </HashRouter>
       </Router>
 
       {/* <Navbar title={'The gym'} HomeText={'Home'} ContactText={'Contact'} PackagesText={'Packages'} FacilitiesText={'Facilities'} AboutText={'About'} modetext={ModeText} mode={Mode} togglemode={togglemode}/>
